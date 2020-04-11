06c8dde691b9f30ebfb48ce160a5fa3c41e7005c
Naveen <naveendevinda163@gmail.com>
2020-04-11 11:37:20 +0530

    Set database connection

--- Sever/src/index.js
+++ Sever/src/index.js
@@ -1,8 +1,13 @@
 import express from "express";
+import connectDB from './config/database';
+import Auth from './routes/auth/auth.routes';
 
 const app = express();
 
-const PORT = process.env.PORT / 5200;
+const PORT = process.env.PORT || 5200;
+
+connectDB();
+app.use(Auth);
 
 app.listen(PORT, () => {
     console.log(`App Started at PORT ${PORT}`);
---
+++ sever/src/config/database.js
@@ -0,0 +1,16 @@
+import { connect } from "mongoose";
+const mongoUrl = "mongodb+srv://naveen:naveen123@cluster0-pjxcl.azure.mongodb.net/test?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+    try {
+        await connect(mongoUrl, { 
+            useNewUrlParser: true ,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to the Database");
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+export default connectDB;
