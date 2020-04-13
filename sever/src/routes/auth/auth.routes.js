43eb7bbc3be75c137f80dfafccfbf4572f004c48
Naveen <naveendevinda163@gmail.com>
2020-04-13 10:51:07 +0530

    Edited

--- sever/src/routes/auth/auth.routes.js
+++ sever/src/routes/auth/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { check } from "express-validator";
-
+import authBodyValidator from './middlewares/auth/authBodyValidator';
 
 const router = Router();
 
@@ -9,10 +9,10 @@ const authValidator = [
     check("password", "password must be at least 6 characters").isLength({
         min: 6
     })
-]
+];
 
-router.post("/login", authValidator, authBodyValidator, async(req, res) => {});
+router.post("/login", authValidator, authBodyValidator, async () => {});
 
-router.post("/register", authValidator, authBodyValidator, async (req, res) => {});
+router.post("/register", authValidator, authBodyValidator, async () => {});
 
 export default router;
