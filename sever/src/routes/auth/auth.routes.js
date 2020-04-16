e1093372cfe751dbfda8a6f3595a86676e58a489
Naveen <naveendevinda163@gmail.com>
2020-04-16 18:18:07 +0530

    Edited

--- sever/src/routes/auth/auth.routes.js
+++ sever/src/routes/auth/auth.routes.js
@@ -3,6 +3,7 @@ import { check } from "express-validator";
 import bcrypt from "bcryptjs";
 import gravatar from "gravatar";
 import { model } from "mongoose";
+import jwt from 'jsonwebtoken';
 
 import authBodyValidator from '../../middlewares/auth/authBodyValidator';
 
@@ -16,8 +17,6 @@ const authValidator = [
         min: 6
     })
 ];
-
-router.post("/login", authValidator, authBodyValidator, async () => {});
 
 router.post("/register", authValidator, authBodyValidator, async (req, res) => {
     try {
@@ -33,11 +32,40 @@ router.post("/register", authValidator, authBodyValidator, async (req, res) => {
         });
         const salt = await bcrypt.genSalt(10);
         password = await bcrypt.hash(password, salt);
-
+        
         const newUser = new User({ email, password, image });
         await newUser.save();
-
+        
         return res.status(200).json(newUser);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ errors: [{ msg: "Internal Server Error" }] });
+    }
+});
+
+router.post("/login", authValidator, authBodyValidator, async (req, res) => {
+    try {
+        let { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: [{ msg: "No User Found on That Email" }] });
+        }
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatched) {
+            return res.status(400).json({ error: [{ msg: "Invalid Password" }] });
+        }
+        const payload = {
+            email: user.email,
+            image: user.image,
+            _id: user._id
+        };
+
+        jwt.sign(payload, "jwtsecrect", { expiresIn: 60 * 60 * 24 }, (err, token) => {
+            if (err) {
+                return res.status(400).json({ errors: [{ msg: "Something went wrong please try again" }] });
+            }
+            return res.status(200).json(token)
+        });
     } catch (err) {
         console.error(err.message);
         return res.status(500).json({ errors: [{ msg: "Internal Server Error" }] });
