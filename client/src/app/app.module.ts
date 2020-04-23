717f7864f91b7684a586fd97c4b20d18a8c69b65
Naveen <naveendevinda163@gmail.com>
2020-04-23 15:26:06 +0530

    Edit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSliderModule } from '@angular/material/slider';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './screens/auth/register/register.component';
-import { LoginComponent } from './screens/auth/login/login.component';
-import { HomeComponent } from './screens/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './screens/auth/register/register.component';
+import { LoginComponent } from './screens/auth/login/login.component';
+import { HomeComponent } from './screens/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
@@ -23,11 +24,7 @@ import { MatCardModule } from '@angular/material/card';
     RegisterComponent,
     LoginComponent,
     HomeComponent,
-    NavbarComponent,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +33,12 @@ import { MatCardModule } from '@angular/material/card';
     ReactiveFormsModule,
     HttpClientModule,
     MatSliderModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
